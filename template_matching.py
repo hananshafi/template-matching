45780acc818595aa0c6af7b630f3367c8e6be0ce
Hanan Gani <37471026+hananshafi@users.noreply.github.com>
2018-11-16 20:16:07 +0530

    Add files via upload

---
+++ template_matching.py
@@ -0,0 +1,20 @@
+import numpy as np
+import argparse
+import imutils
+import glob
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--template", required=True, help="path to template image")
+ap.add_argument("-i", "--images", required=True, help="path to image")
+# ap.add_argument("-v", "--visualize",help="Flag indicating whether or not to visualize each iteration")
+args = vars(ap.parse_args())
+
+# load the image image, convert it to grayscale, and detect edges
+template = cv2.imread(args["template"])
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+template = cv2.Canny(template, 50, 200)
+(tH, tW) = template.shape[:2]
+cv2.imshow("Template", template)
+cv2.waitKey(0)
